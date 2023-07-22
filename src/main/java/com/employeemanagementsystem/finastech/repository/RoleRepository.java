e255eb455d2c195cafada4b570d22fd462bafb49
Okan Böke <okanbokee@gmail.com>
2023-07-23 01:47:09 +0300

    role repo eklendi

---
+++ src/main/java/com/employeemanagementsystem/finastech/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.employeemanagementsystem.finastech.repository;
+
+import com.employeemanagementsystem.finastech.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String roleName);
+
+
+
+}
